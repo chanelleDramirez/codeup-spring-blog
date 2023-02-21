9ca189d49cb9ddc75944d60a531b42bf33c2e4d0
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-02-21 14:07:39 -0600

    PostController completed

--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -2,14 +2,27 @@ package com.codeup.codeupspringblog;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostController {
 
-    @GET("/posts")
+    @GetMapping("/posts")
     @ResponseBody
-    public String posts(){
+    public String posts() {
         return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String id() {
+        return "view an individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String create() {
+        return "view the form for creating a post";
     }
 }
